d98b012c42c12f9b954887feeb4c1d5d8b83c180
Victor Teran Herrera <vteran93@yahoo.es>
2019-08-03 17:41:06 -0500

    Remove pyc code

--- src/houselandbog/imagesuploader/Providers/ConnectionProvider.py
+++
@@ -1,8 +0,0 @@
-"""
-Esta clase debe decidir el driver con el cual se va a conectar
-y retornar la instancia correspondiente
-"""
-class ConnectionProvider(object):
-    def __init__(self, *args):
-        super(ConnectionProvider, self).__init__(*args))
-    
